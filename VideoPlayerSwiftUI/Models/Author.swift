92702c1d7164f57aae838cf2d7bdb74caddefaaa
Madusanka Prabath <imm.prabath@gmail.com>
2024-01-15 00:42:15 -0330

    Initial Commit
    * Created new repo and copied necessary files
    * Added new assets
    * Added new project structure
    * Added new models

---
+++ VideoPlayerSwiftUI/Models/Author.swift
@@ -0,0 +1,8 @@
+//
+//  Author.swift
+//  VideoPlayerSwiftUI
+//
+//  Created by Madusanka Prabath on 2024-01-14.
+//
+
+import Foundation
---
+++ VideoPlayerSwiftUI/Models/Video.swift
@@ -0,0 +1,8 @@
+//
+//  Video.swift
+//  VideoPlayerSwiftUI
+//
+//  Created by Madusanka Prabath on 2024-01-14.
+//
+
+import Foundation
